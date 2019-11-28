dc5986b64baf9492d21afc02fafb96a7f4cec420
ozhangulal <ozhangulal@outlook.com>
2019-11-28 14:28:13 +0300

    using bootstrap in dot net core mvc
    using bootstrap in dot net core mvc examine with details

---
+++ usingBootstrapInCoreMVC/usingBootstrapInCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace usingBootstrapInCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
